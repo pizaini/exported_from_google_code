d13f304ec6fe6355642a9bc519f89f2d0f4bf361
Pizaini <zheiner.taurus@gmail.com>
2012-02-11 01:52:38 +0700

    This is Hello Java....
    Change-Id: Iddff8789dd1446c3a6f4cdd44848dcd2a7412052
    Signed-off-by: Pizaini <zheiner.taurus@gmail.com>

---
+++ HelloJava/src/points/Point.java
@@ -0,0 +1,16 @@
+package points;
+
+public class Point {
+	private String TAG =  "Point";
+	
+	public Point() {
+	
+	}
+	
+	public String getTAG() {
+		return TAG;
+	}
+	public void setTAG(String tAG) {
+		TAG = tAG;
+	}
+}
